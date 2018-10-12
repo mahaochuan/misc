ce3fd758d3ce61ba6178b2c3d49f57ca2a01c59f
mahaochuan <1162940726@qq.com>
2018-10-12 13:45:32 +0800

    poi excel导出

---
+++ misc/src/main/java/com/xml/common/service/ExportExcelService.java
@@ -0,0 +1,59 @@
+package com.xml.common.service;
+
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * Excel导出工具
+ * @author mhc
+ *
+ * @param <T>
+ */
+public interface ExportExcelService<T> {
+
+
+
+	/**
+	 * 这是一个通用的方法，利用了JAVA的反射机制，可以将放置在JAVA集合中并且符号一定条件的数据以EXCEL 的形式输出到指定IO设备上
+	 * 
+	 * @param title
+	 *            表格标题名
+	 * @param headers
+	 *            表格属性列名数组
+	 * @param dataset
+	 *            需要显示的数据集合,集合中一定要放置符合javabean风格的类的对象。此方法支持的
+	 *            javabean属性的数据类型有基本数据类型及String,Date,byte[](图片数据)
+	 * @param out
+	 *            与输出设备关联的流对象，可以将EXCEL文档导出到本地文件或者网络中
+	 * @param pattern
+	 *            如果有时间数据，设定输出格式。默认为"yyy-MM-dd"
+	 */
+	public void exportExcel(String title, String[] headers, Collection<T> dataset, OutputStream out, String pattern);
+	
+	
+	
+	
+	
+	
+	/**
+	 * 这是一个通用的方法，利用了JAVA的反射机制，可以将放置在JAVA集合中并且符号一定条件的数据以EXCEL 的形式输出到指定IO设备上
+	 * 
+	 * @param title
+	 *            表格标题名
+	 * @param headers
+	 *            表格属性列名数组
+	 * @param dataset
+	 *            需要显示的数据集合,集合中一定要放置符合javabean风格的类的对象。此方法支持的
+	 *            javabean属性的数据类型有基本数据类型及String,Date,byte[](图片数据)
+	 * @param out
+	 *            与输出设备关联的流对象，可以将EXCEL文档导出到本地文件或者网络中
+	 * @param pattern
+	 *            如果有时间数据，设定输出格式。默认为"yyy-MM-dd"
+	 */
+	public void exportExcel(String title, List<String> headers, List<Map<String,Object>> dataset, OutputStream out, String pattern);
+	
+
+}
